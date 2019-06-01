daf2161ac43dbb9328e514a15604f163a402928b
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-01 18:03:54 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,15 +1,3 @@
 def my_select(collection)
-  if block_given?
-    new_array = []
-    i = 0
- 
-    while i < array.length
-      new_array << yield(array[i])
-      i = i + 1
-    end
- 
-    new_array
-  else
-    puts "Hey! No block was given!"
-  end
+ # your code here!
 end
