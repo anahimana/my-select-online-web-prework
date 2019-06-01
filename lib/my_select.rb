33667d0e6cbd447750223d08799c03aae397bfb1
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-01 18:10:24 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,7 +3,7 @@ def my_select(collection)
  # your code here!
  new_coll = []
  collection.each do |item, index|
-   new_coll << yield(item[i])
+   new_coll << yield(item[inde])
  end
  new_coll
 end
